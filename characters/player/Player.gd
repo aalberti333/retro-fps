7a76c1ec648529b19d6492bf28b9db38a401aae0
aalberti333 <aalberti333@protonmail.com>
2021-04-21 12:58:41 -0600

    begin HealthManager

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -4,16 +4,25 @@ export var mouse_sens = 0.5
 
 onready var camera = $Camera
 onready var character_mover = $CharacterMover
+onready var health_manager = $HealthManager
+
+var dead = false
 
 func _ready():
 	# makes mouse invisible, and keeps it at the center of the screen
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	character_mover.init(self)
+	health_manager.init()
+	# How to connect to signal via code instead of through GUI
+	health_manager.connect("dead", self, "kill")
 
 func _process(delta):
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
-		
+	
+	if dead:
+		return
+	
 	var move_vec = Vector3()
 	if Input.is_action_pressed("move_forwards"):
 		move_vec += Vector3.FORWARD
@@ -33,3 +42,13 @@ func _input(event):
 		rotation_degrees.y -= mouse_sens * event.relative.x
 		camera.rotation_degrees.x -= mouse_sens * event.relative.y
 		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
+
+func hurt(damage, dir):
+	health_manager.hurt(damage, dir)
+
+func heal(amount):
+	health_manager.heal(amount)
+
+func kill():
+	dead = true
+	character_mover.freeze()
